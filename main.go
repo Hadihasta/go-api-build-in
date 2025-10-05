2be16491b6ad35e167d342119731b72431c9f614
hadiasta <hijrihadi@gmail.com>
2025-10-05 13:46:14 +0700

    feat : add login server http handler

--- main.go
+++ main.go
@@ -1,1 +1,26 @@
 package main
+
+import (
+	"log"
+	"net/http"
+)
+
+// define server struct
+type server struct{
+	addr string
+}
+
+// implement ServeHTTP method for server struct
+func (s *server) ServeHTTP(w http.ResponseWriter, r *http.Request) {
+	// handle requests here
+	w.Write([]byte("hello from server"))
+}
+
+// use server struct in main function
+func main() {
+	s := &server{addr: ":8080"}
+ if err := 	http.ListenAndServe(s.addr, s); err != nil {
+	log.Fatal(err)
+ }
+
+}
