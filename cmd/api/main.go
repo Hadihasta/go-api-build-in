5b1f396e80cb001ed8443bc503d0bbb543e2cc8e
hadiasta <hijrihadi@gmail.com>
2025-10-07 21:22:03 +0700

    add connection to database

--- cmd/api/main.go
+++ cmd/api/main.go
@@ -3,6 +3,7 @@ package main
 import (
 	"log"
 
+	"github.com/hadihasta/go-api-build-in/internal/db"
 	"github.com/hadihasta/go-api-build-in/internal/env"
 	"github.com/hadihasta/go-api-build-in/store"
 )
@@ -10,18 +11,32 @@ import (
 func main() {
 
 	cfg := config{
-		addr: env.GetString("ADDR",":8080"),
+		addr: env.GetString("ADDR", ":8080"),
+		db: dbConfig{
+			addr:         env.GetString("DB_ADDR", "postgres://user:root@localhost/myapps?sslmode=disable"),
+			maxOpenConns: env.GetInt("DB_MAX_OPEN_CONNS", 30),
+			maxIdleConns: env.GetInt("DB_MAX_IDLE_CONNS", 30),
+			maxIdleTime:  env.GetString("DB_MAX_IDLE_TIME", "15m"),
+		},
 	}
 
-	store := store.NewStorage(nil)
+	db, err := db.New(
+		cfg.db.addr,
+		cfg.db.maxOpenConns,
+		cfg.db.maxIdleConns,
+		cfg.db.maxIdleTime,
+	)
 
+	if err != nil {
+		log.Panic(err)
+	}
+
+	store := store.NewStorage(db)
 
 	app := &application{
 		config: cfg,
-		store: store,
+		store:  store,
 	}
-	
-
 
 	mux := app.mount()
 
